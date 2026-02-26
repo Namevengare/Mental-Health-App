6300670dfd06ceaa5d6f863ec2ba72078c93b9ed
Isaza <miguel.isaza@valeant.com>
2026-02-26 16:12:11 -0500

    Some header added

--- app/src/main/java/com/example/mentalhealthapp/MainActivity.kt
+++ app/src/main/java/com/example/mentalhealthapp/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.mentalhealthapp.ui.theme.MentalHealthAppTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +34,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    MentalHealthBanner()
                 }
             }
         }
@@ -45,3 +56,32 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun MentalHealthBanner(modifier: Modifier = Modifier){
+    val icon = painterResource(R.drawable.fruit_15467270)
+    Row(modifier) {
+        Image(
+            painter = icon,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(20.dp)
+        )
+        Column{
+            Text(
+                text = "Mental Health app",
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = "Tu bienestar mental🫶",
+                fontSize = 5.sp
+            )
+        }
+
+    }
+
+}
+
